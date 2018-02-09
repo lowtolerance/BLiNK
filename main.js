9d9d3f3cad320f7171e4c165ee1f7218843b0ff0
Rob Nixon <robnixon515@gmail.com>
2018-02-09 11:26:21 -0500

    Added missing subscriber :\

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var BLiNK = require('BLiNK'),
 var client = mqtt.connect('mqtt://127.0.0.1');
 
 client.on('connect', function () {
+    client.subscribe('ticker');
     client.publish('ticker', '@0#0!@1#1!@2#2!@3#3!@4#4!@5#5!@6#6!@7#7!');
 });
 
