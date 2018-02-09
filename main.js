1ff724cbb7a1729b70b0ebd01c6a03748b2c71c8
Rob Nixon <robnixon515@gmail.com>
2018-02-09 11:07:26 -0500

    Removed redundant variable declaration

--- main.js
+++ main.js
@@ -2,7 +2,4 @@ var convert = require('color-convert'),
     Blinkt = require('node-blinkt/Blinkt'),
     BLiNK = require('BLiNK');
 
-leds = new Blinkt();
-leds.setup();
-
 BLiNK.stringParser('@0#0!@1#1!@2#2!@3#3!@4#4!@5#5!@6#6!@7#7!');
