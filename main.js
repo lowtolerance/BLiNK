f10e72edf818a8a3b82ea218f77cfc6be6a7c57c
Rob Nixon <robnixon515@gmail.com>
2018-02-09 11:22:47 -0500

    mqtt client missing protocol in URI

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 var BLiNK = require('BLiNK'),
-    mqtt = require('mqtt')
+    mqtt = require('mqtt');
 
-var client = mqtt.connect('127.0.0.1');
+var client = mqtt.connect('mqtt://127.0.0.1');
 
 client.on('connect', function () {
     client.publish('ticker', '@0#0!@1#1!@2#2!@3#3!@4#4!@5#5!@6#6!@7#7!');
