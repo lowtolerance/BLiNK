f20cae8a88deed50896ac6b7e0eb69a70d034230
Rob Nixon <robnixon515@gmail.com>
2018-02-09 13:22:33 -0500

    Removed unused line of code

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ var client = mqtt.connect('mqtt://127.0.0.1');
 
 client.on('connect', function () {
     client.subscribe('ticker');
-    //client.publish('ticker', '@0#0!@1#1!@2#2!@3#3!@4#4!@5#5!@6#6!@7#7!');
 });
 
 client.on('message', function (topic, message) {
